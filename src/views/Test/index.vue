330c7c9395e8ec7f2aca8cd72c78f81f6f11009c
LawLv <95074442+LawLv@users.noreply.github.com>
2023-07-19 20:40:59 +0800

    wudi

--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="app-container">
-      <div class="filter-container">
+    <div class="mixin-components-container">
         <div class="container">
           <h1 class="page-title">My Project</h1>
           <div class="project-grid">
@@ -18,14 +17,141 @@
             </router-link>
           </div>
         </div>
-      </div>
+        <el-row>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+            <span>Buttons</span>
+            </div>
+            <div style="margin-bottom:50px;">
+            <el-col :span="4" class="text-center">
+                <router-link class="pan-btn blue-btn" to="/documentation/index">
+                Documentation
+                </router-link>
+            </el-col>
+            <el-col :span="4" class="text-center">
+                <router-link class="pan-btn light-blue-btn" to="/icon/index">
+                Icons
+                </router-link>
+            </el-col>
+            <el-col :span="4" class="text-center">
+                <router-link class="pan-btn pink-btn" to="/excel/export-excel">
+                Excel
+                </router-link>
+            </el-col>
+            <el-col :span="4" class="text-center">
+                <router-link class="pan-btn green-btn" to="/table/complex-table">
+                Table
+                </router-link>
+            </el-col>
+            <el-col :span="4" class="text-center">
+                <router-link class="pan-btn tiffany-btn" to="/example/create">
+                Form
+                </router-link>
+            </el-col>
+            <el-col :span="4" class="text-center">
+                <router-link class="pan-btn yellow-btn" to="/theme/index">
+                Theme
+                </router-link>
+            </el-col>
+            </div>
+        </el-card>
+        </el-row>
+
+        <el-row :gutter="20" style="margin-top:50px;">
+            <el-col :span="6">
+                <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>Material Design 的input</span>
+                </div>
+                <div style="height:100px;">
+                    <el-form :model="demo" :rules="demoRules">
+                    <el-form-item prop="title">
+                        <md-input v-model="demo.title" icon="el-icon-search" name="title" placeholder="输入标题">
+                        标题
+                        </md-input>
+                    </el-form-item>
+                    </el-form>
+                </div>
+                </el-card>
+            </el-col>
+
+            <el-col :span="6">
+                <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>图片hover效果</span>
+                </div>
+                <div class="component-item">
+                    <pan-thumb width="100px" height="100px" image="https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191">
+                    vue-element-admin
+                    </pan-thumb>
+                </div>
+                </el-card>
+            </el-col>
+
+            <el-col :span="6">
+                <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>水波纹 waves v-directive</span>
+                </div>
+                <div class="component-item">
+                    <el-button v-waves type="primary">
+                    水波纹效果
+                    </el-button>
+                </div>
+                </el-card>
+            </el-col>
+
+            <el-col :span="6">
+                <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>hover text</span>
+                </div>
+                <div class="component-item">
+                    <mallki class-name="mallki-text" text="vue-element-admin" />
+                </div>
+                </el-card>
+            </el-col>
+        </el-row>
+        <div class="dashboard-container">
+            <component :is="currentRole" />
+        </div>
     </div>
   </template>
   
   <script>
+  import { mapGetters } from 'vuex'
+  import adminDashboard from './admin'
+  import PanThumb from '@/components/PanThumb'
+  import MdInput from '@/components/MDinput'
+  import Mallki from '@/components/TextHoverEffect/Mallki'
+  import waves from '@/directive/waves/index.js' // 水波纹指令
+
   export default {
+    components: {
+    PanThumb,
+    MdInput,
+    Mallki,
+    adminDashboard
+    },
+    directives: {
+    waves
+    },
     data() {
+        const validate = (rule, value, callback) => {
+            if (value.length !== 6) {
+                callback(new Error('请输入六个字符'))
+            } else {
+                callback()
+            }
+            }
       return {
+        currentRole: 'adminDashboard',
+        demo: {
+        title: ''
+      },
+      demoRules: {
+        title: [{ required: true, trigger: 'change', validator: validate }]
+      },
         projects: [
           { id: 1, title: 'Project 1', color: this.getRandomLightColor() },
           { id: 2, title: 'Project 2', color: this.getRandomLightColor() },
@@ -45,6 +171,16 @@
         return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
       },
     },
+    computed: {
+        ...mapGetters([
+        'roles'
+        ])
+    },
+    created() {
+        if (!this.roles.includes('admin')) {
+        this.currentRole = 'editorDashboard'
+        }
+    }
   };
   </script>
   
@@ -92,5 +228,13 @@
     background-color: green;
     color: white;
   }
+  .mixin-components-container {
+  background-color: #eefafc;
+  padding: 30px;
+  min-height: calc(100vh - 84px);
+}
+.component-item{
+  min-height: 100px;
+}
   </style>
   
